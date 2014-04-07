f5beb5564479b50b0cece5ba57593cd00bd6dfdd
Mista-Tea <thatguythomas@hotmail.com>
2014-04-07 16:36:17 -0600

    Including license to make copying easier

--- netwrapper.lua
+++ netwrapper.lua
@@ -6,7 +6,27 @@
 		Mista-Tea ([IJWTB] Thomas)
 	
 	License:
-		The MIT License (copy/modify/distribute freely!)
+		The MIT License (MIT)
+
+		Copyright (c) 2014 Mista-Tea
+
+		Permission is hereby granted, free of charge, to any person obtaining a copy
+		of this software and associated documentation files (the "Software"), to deal
+		in the Software without restriction, including without limitation the rights
+		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+		copies of the Software, and to permit persons to whom the Software is
+		furnished to do so, subject to the following conditions:
+
+		The above copyright notice and this permission notice shall be included in all
+		copies or substantial portions of the Software.
+
+		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+		SOFTWARE.
 			
 	Changelog:
 		- April 7th, 2014:	Created
@@ -94,7 +114,7 @@ elseif ( CLIENT ) then
 	net.Receive( "NetWrapper", function( len )
 		local ent = net.ReadEntity()
 		local key = net.ReadString()
-		local id  = net.ReadUInt( 8 )   -- read the prepended type ID that was written automatically by net.WriteType(*)
+		local id  = net.ReadUInt( 8 )  -- read the prepended type ID that was written automatically by net.WriteType(*)
 		local val = net.ReadType( id ) -- read the data using the corresponding type ID
 
 		ent:SetNetVar( key, val )
