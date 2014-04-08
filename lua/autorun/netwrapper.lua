ae4dd3e20b318e84856877c677020ee0acc2ce10
Mista-Tea <ryan_zauros@hotmail.com>
2014-04-07 18:32:16 -0600

    Rewording file description

--- lua/autorun/netwrapper.lua
+++ lua/autorun/netwrapper.lua
@@ -9,25 +9,35 @@
 		The MIT License (copy/modify/distribute freely!)
 	
 	File description:
-		A simple library for networking data on entities between server and client
-		with the use of net messages instead of the standard networking libraries.
+		The netwrapper library is a simple wrapper over the net library to provide lightweight 
+		 networking without hassle of dealing with raw net messages or using ENTITY:SetNW* functions.
 		
-		Where other libraries are constantly networking data even when the data isn't
-		changing, this library will only sync the data when the values change.
+		The netwrapper library only networks data on an entity when the data is changed or added with
+		 ENTITY:SetNetVar( key, value ) from the server. By broadcasting net messages only when 
+		 the data changes, this library has a relatively low impact on network traffic.
+
+		The netwrapper library takes away the hassle of handling net messages directly 
+		 by using a single net message to automatically read and write the corresponding 
+		 values you are networking. Once these values have been broadcasted, all connected 
+		 clients will be able to retrieve the values like you would with the standard networking libraries.
 		
-		This should theoretically provide a lightweight networking solution without
-		heavy network traffic.
-		
-		To network a value on an entity, simply use the ENTITY:SetNetVar function.
+		Setting networked values:
+			ENTITY:SetNetVar( key, value )
+			
+		Getting networked values:
+			ENTITY:GetNetVar( key, default )
+			
+		Where 'default' is the default value you would like returned if the key doesn't exist.
+		 If default isn't provided and the key doesn't exist, nil will be returned.
 		
 		[EXAMPLE] To set a networked variable on a connected player, you could
-		do the following on from your server console:
+		 do the following on from your server console:
 			
 			Entity(1):SetNetVar( "TeamName", "Example Team" )
 			
 		Now the player (Entity(1)) has a networked variable with the key "TeamName" 
-		and the value "Example Team". We can retrieve this value on both the server
-		and client by doing the following:
+		 and the value "Example Team". We can retrieve this value on both the server
+		 and client by doing the following:
 		
 			local teamName = Entity(1):GetNetVar( "TeamName" )
 			
@@ -35,12 +45,13 @@
 		
 			Entity(1):SetNetVar( "TeamName", "Another Example" )
 		
-		Then any subsequent calls to Entity(1):GetNetVar( "TeamName" ) will 
-		return "Another Example".
+		Any subsequent calls to Entity(1):GetNetVar( "TeamName" ) will 
+		 return "Another Example".
 	
 	Changelog:
 		- March 9th, 2014:	Created
 		- April 5th, 2014:	Added to GitHub
+		- April 7th, 2014:	Reworded file description
 --------------------------------------------------------------------------------]]
 
 print( "[NetWrapper] Initializing netwrapper library" )
