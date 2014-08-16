cbe9139550a61fcb34d55cbad9e0a272f76ef4ab
Mista-Tea <thatguythomas@hotmail.com>
2014-08-16 15:37:26 -0600

    Added better examples

--- lua/netwrapper/example.lua
+++ lua/netwrapper/example.lua
@@ -9,104 +9,101 @@
 		The MIT License (copy/modify/distribute freely!)
 		
 	Changelog:
-		- March 9th, 2014:	Created
-		- April 5th, 2014:	Added to GitHub
+		- March   9th, 2014:    Created
+		- April   5th, 2014:    Added to GitHub
+		- August 16th, 2014:    Rewrote example for Net Vars / Net Requests
 ----------------------------------------------------------------------------]]
 
---[[
-	EXAMPLE:
-	 
-	If you use a round-based gamemode with teams, you may have a case where you 
-	 want to network each player's custom team name so it can be drawn in a HUDPaint hook.
+-- In this example, we will assign a player's name to the prop they spawn in PlayerSpawnedProp.
+-- We'll use Net Vars in the first example to show how they automatically network themselves,
+-- and then show how Net Requests can be used to ask for the value to be networked manually.
+
+
+
+
+
+
+--[[-------------------------------------------]]--
+--                    Net Vars
+--[[-------------------------------------------]]--
+
+if ( SERVER ) then
 	
-	To test out the functionality of the netwrapper library, choose a player entity.
+	-- when a player spawns a prop, assign the owner's name to it with ent:SetNetVar()
+	hook.Add( "PlayerSpawnedProp", "AssignOwner", function( ply, mdl, ent )
 	
-	If you or someone else is currently on your server, you can use Entity(1) to get a player entity.
+		ent:SetNetVar( "Owner", ply:Nick() ) -- stores and networks the value to clients
+
+	end )
 	
-	To network this player's Team Name, we can do the following:
-]]
-
-Entity(1):SetNetVar( "TeamName", "Example Team" )
-
---[[
-	We've just networked the TeamName of this player between the server and any connected clients.
+elseif ( CLIENT ) then
 	
-	To retrieve this value on either the server or client, do the following:
-]]
-
-Entity(1):GetNetVar( "TeamName" )
-
---[[
-	This will return the string, "Example Team".
+	-- draw the owner's name of any entity we look at during HUDPaint
+	hook.Add( "HUDPaint", "DrawOwner", function()
 	
-	You can change the type of value stored at the "TeamName" key to amy value type supported by the
-	 net library.
-	 
-	 Here are some examples:
-]]
-
-Entity(1):SetNetVar( "TeamName", Vector(123,123,123) ) -- now TeamName returns a vector
-
-Entity(1):SetNetVar( "TeamName", Color(255,0,0,100) ) -- now a table
-
-Entity(1):SetNetVar( "TeamName", 1000 ) -- now a number
-
---[[	Questions & Answers
-	
-	Q: What sort of data can I network with this library?
-	
-	A: Since this is a wrapper library over the standard net library, all limitations of the net library apply here.
-		For example, you can't network functions or user data.
+		if ( !IsValid( LocalPlayer() ) ) then return end
 		
-		What you CAN network:
-			nil
-			string
-			number
-			table
-			bool
-			entity
-			vector
-			angle
+		local ent = LocalPlayer():GetEyeTrace().Entity -- get the entity we're looking at
+		if ( !IsValid( ent ) or ent:IsPlayer() ) then return end
+		
+		local owner = ent:GetNetVar( "Owner", "N/A" ) -- get the owner's name, but if it hasn't been networked to us yet, use N/A
+		
+		surface.SetFont( "Trebuchet MS" )
+		local w, h = surface.GetTextSize( owner )
+		local x = ScrW() - w - 15
+		local y = ScrH() / 2.3
+		
+		draw.SimpleText( owner, "Trebuchet MS", x, y, color_white, 0, 0 )
 	
-	-----------------------------------------------------------------------------------------------------------------------
+	end )
+	-- As soon as the server called ent:SetNetVar( "Owner", ply:Nick() ), the owner's name would be broadcasted to all clients.
+	-- The moment we look at the prop after it has been spawned, we'll be able to get the networked name with ent:GetNetVar( "Owner" ).
 	
-	Q: How often is the data networked?
-	
-	A: Every time you use SetNetVar on an entity, the data will be networked to any clients using net messages.
-	
-	Instead of constantly syncing the data even when the data hasn't changed (like the standard GMod networking libraries do),
-	 this will only send out a broadcasted net message when the data changes, theoretically reducing network traffic.
-	 
-	If you set a value on a player and then change the value 5 minutes later, the data will have been broadcasted only 2 times
-	 over the span of the last 5 minutes.
-	
-	However, this does mean that if you use SetNetVar in a think hook, it will be broadcasting net messages on every think.
-	
-	As with any other function, be sure to set networked data only as often as you need to. Think hooks should typically be
-	 avoided if you plan on networking large amounts of data on large amounts of entities/players.
-	
-	-----------------------------------------------------------------------------------------------------------------------
-	
-	Q: What happens when clients connect after the data has already been broadcasted?
+end
 
-	A: When a client fully initalizes on the server (during the InitPostEntity hook), they will send a net message to
-	 the server that requests any data that is currently being networked on entities.
-	 
-	So, if a new player was to connect after we've already been networking Player 1's TeamName, the connecting client will
-	 receive the data that was networked on Player 1. Once they've received the value, using Entity(1):GetNetVar( "TeamName" )
-	 will return the last networked value we set on them.
-	 
-	-----------------------------------------------------------------------------------------------------------------------
-	 
-	Q: What happens to the networked data on a player that disconnected, or an entity that was removed?
+
+
+
+
+
+--[[-------------------------------------------]]--
+--                 Net Requests
+--[[-------------------------------------------]]--
+
+if ( SERVER ) then
 	
-	A: When a player disconnects or an entity is removed, their state will be changed to NULL in our table that holds
-	 all of the entities and values currently being networked. This occurs automatically.
+	-- when a player spawns a prop, assign the owner's name to it with ent:SetNetRequest()
+	hook.Add( "PlayerSpawnedProp", "AssignOwner", function( ply, mdl, ent )
 	
-	The next time that a player connects and asks the server for all networked entities, the server will loop through
-	 all of the entities and remove any NULL references that it finds, including any disconnected players or removed 
-	 entities.
-	 
-	Because of this automatic sanitization of the netwrapper.ents table, you don't have to worry about sending NULL references
-	 to your connecting players.
-]]
+		ent:SetNetRequest( "Owner", ply:Nick() ) -- stores the value but does not network it
+
+	end )
+	
+elseif ( CLIENT ) then
+	
+	-- draw the owner's name of any entity we look at during HUDPaint
+	hook.Add( "HUDPaint", "DrawOwner", function()
+	
+		if ( !IsValid( LocalPlayer() ) ) then return end
+		
+		local ent = LocalPlayer():GetEyeTrace().Entity -- get the entity we're looking at
+		if ( !IsValid( ent ) or ent:IsPlayer() ) then return end
+		
+		local owner = ent:GetNetRequest( "Owner" ) -- get the owner's name
+		if ( !owner ) then ent:SendNetRequest( "Owner" ) end -- if the owner's name hasn't been networked to us yet, send a Net Request that asks for it
+		
+		owner = owner or "N/A" -- until we have the actual owner's name, we can just use N/A
+		
+		surface.SetFont( "Trebuchet MS" )
+		local w, h = surface.GetTextSize( owner )
+		local x = ScrW() - w - 15
+		local y = ScrH() / 2.3
+		
+		draw.SimpleText( owner, "Trebuchet MS", x, y, color_white, 0, 0 )
+	
+	end )
+	-- when ent:SendNetRequest( "Owner" ) is used on the client, the client will ask the server if it has any data stored on the entity at the key "Owner"
+	-- if it does, it will reply with the value and the client will automatically use ent:SetNetRequest( "Owner", value ) so that any
+	-- subsequent calls to ent:GetNetRequest( "Owner" ) returns the value
+	
+end
